ac22ad5a9d04b2b1db57beca1139e0f427437333
tsmx <dev@tsmx.net>
2022-07-01 20:30:56 +0200

    TS type definitions test improved

--- human-readable.test-d.ts
+++ human-readable.test-d.ts
@@ -1,3 +1,13 @@
-import { availableSizes } from './human-readable';
+import { expectType } from 'tsd';
+import { availableSizes, fromBytes, calcFromTo, HROptionsType } from './human-readable';
 
-availableSizes();
+const testOptions: HROptionsType = {
+    mode: 'IEC',
+    numberOnly: false
+}
+
+expectType<string[]>(availableSizes());
+
+expectType<string>(fromBytes(1024, testOptions));
+
+expectType<string>(calcFromTo(1024, 'KBYTE', 'MBYTE', undefined));
