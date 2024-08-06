7a2e9006c79731d1c322d0d7c676893901064b68
tsmx <dev@tsmx.net>
2024-08-06 20:59:32 +0200

    eslint updated to 9.8

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+const { configs } = require('@eslint/js');
+const jest = require('eslint-plugin-jest');
+
+let _test = 0;
+
+module.exports = [
+    configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2018,
+            sourceType: 'commonjs',
+            globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' }
+        },
+        rules: {
+            semi: 'error',
+            quotes: ['error', 'single'],
+            indent: ['error', 4, { 'SwitchCase': 1 }],
+            'no-unused-vars':
+                [
+                    'warn',
+                    {
+                        'varsIgnorePattern': '^_',
+                        'args': 'after-used',
+                        'argsIgnorePattern': '^_'
+                    }
+                ]
+        },
+        ignores: ['coverage/', 'conf/']
+    },
+    {
+        files: ['test/*.js'],
+        ...jest.configs['flat/recommended'],
+        rules: {
+            ...jest.configs['flat/recommended'].rules
+        }
+    }
+];
